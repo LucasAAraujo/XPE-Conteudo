c536f28442f038ab1517f715b02b6f57e111ac22
LucasAAraujo <lucasalves3.0@hotmail.com>
2025-04-30 08:23:00 -0300

    Aula: Ordenação de dados

--- ordenacao_dados.py
+++ ordenacao_dados.py
@@ -8,5 +8,25 @@ dados = {'Nome': ['Maria','Joao', 'Ana', 'Pedro','Joao'],
 df = pd.DataFrame(dados)
 print(df.head())
 
+print('########################')
+
 df_agrupado = df.groupby('Categoria')['Vendas'].sum()
 print(df_agrupado)
+
+print('########################')
+
+#df = df.assign(Ano_Nascimento = lambda x: 2025 - x['Idade'])
+#print(df.head())
+
+print('########################')
+
+dados_2 = {'Grupo': ['A', 'B', 'A', 'B'],
+           'Valor': [10, 20, 30, 40]}
+
+df = pd.DataFrame(dados_2)
+print(df)
+
+print('########################')
+
+df['Media_Grupo'] = df.groupby('Grupo')['Valor'].transform('mean')
+print(df.head)
